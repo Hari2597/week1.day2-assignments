a3640cf5fbde051fcf2c37c84da823e4e385ad7d
Hari2597 <117000965+Hari2597@users.noreply.github.com>
2022-11-02 21:37:04 +0530

    Add files via upload

---
+++ NegativetoPositive.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class NegativetoPositive {
+	public static void main(String[] args) {
+			
+			int a =-20;
+			
+			if (a < 0) {
+			    a = a * -1;
+			}
+	System.out.println(a);
+	}
+}
