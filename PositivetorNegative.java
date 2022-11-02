99cba8d09a9289b1815ed98744da6fabc5f726eb
Hari2597 <117000965+Hari2597@users.noreply.github.com>
2022-11-02 21:38:32 +0530

    Add files via upload

---
+++ PositivetorNegative.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class PositivetorNegative {
+	public static void main(String[] args) {
+		int number=35;
+		
+		if (number>1) {
+			System.out.println("positive number=" +number);	
+		}
+		
+		else if (number<1) {
+			System.out.println("negative number=" +number);
+		}
+		
+		else {
+		System.out.println("zero" +number);
+		
+	}
+
+		}
+	}
+
