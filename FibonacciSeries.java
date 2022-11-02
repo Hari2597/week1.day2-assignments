237b98fa9aebb26521fd48644e2b81854b24b481
Hari2597 <117000965+Hari2597@users.noreply.github.com>
2022-11-02 21:41:41 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class FibonacciSeries {
+public static void main(String[] args) {
+		
+		int n =8, num1=0, num2=1, sum;
+		for (int i = 1; i < n; i++) {
+			System.out.println(num2);
+			sum = num1+num2;
+			num1 =num2;
+			num2=sum;
+			
+	  }
+	}
+  }
+
