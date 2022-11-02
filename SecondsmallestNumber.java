65e98f350c87abe4add9f3c642640a97725b562a
Hari2597 <117000965+Hari2597@users.noreply.github.com>
2022-11-02 21:39:47 +0530

    Add files via upload

---
+++ SecondsmallestNumber.java
@@ -0,0 +1,9 @@
+package week1.day2;
+
+public class SecondsmallestNumber {
+	public static void main(String[] args) {
+			int a[]= {23,45,67,89,22};
+			System.out.println(a[1]);
+		    }
+	      }
+        
